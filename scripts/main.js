feb5c60cd78d3b2bc0f617b79e1b51a61795ad0f
= <john.michael.shanley@gmail.com>
2015-04-11 15:18:42 -0400

    maintain image centering and size

--- scripts/main.js
+++ scripts/main.js
@@ -6,27 +6,26 @@
 
     var imageUrl = 'http://54.173.76.9/FI9821W_C4D655392937/snap/current.jpg?rand=' + rand;
 
-    var image = document.getElementById('armory-cam');
+    var cam = document.getElementById('armory-cam');
 
-    image.src = imageUrl;
+    var urlString = "url('" + imageUrl + "')";
 
-    window.addEventListener('load', maintainSize);
+    cam.style.backgroundImage = urlString;
+
     window.addEventListener('resize', maintainSize);
+    maintainSize();
 
   });
 
   function maintainSize() {
-    var container = document.getElementById('image-container');
-    var windowWidth = window.innerWidth;
-    var windowHeight = window.innerHeight;
-
-    console.log('maintaining size...');
-    console.log('width', windowWidth);
-    console.log('height', windowHeight);
-
-    container.style.width = windowWidth + 'px';
-    container.style.height = windowHeight + 'px';
+    var cam = document.getElementById('armory-cam');
+    var w = window.innerWidth,
+        h = window.innerHeight;
+    cam.style.height = h + 'px';
+    cam.style.width = w + 'px';
   }
+
+
 
 
 })()
