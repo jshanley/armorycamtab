201723799e10620845eae73b9e1da67cabc4f47b
= <john.michael.shanley@gmail.com>
2015-04-11 15:01:11 -0400

    mhm

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,32 @@
 (function() {
 
-  var rand = Math.random();
+  document.addEventListener('DOMContentLoaded', function () {
 
-  var imageUrl = 'http://54.173.76.9/FI9821W_C4D655392937/snap/current.jpg?rand=' + rand;
+    var rand = Math.random();
 
-  var image = document.getElementById('armory-cam');
+    var imageUrl = 'http://54.173.76.9/FI9821W_C4D655392937/snap/current.jpg?rand=' + rand;
 
-  image.src = imageUrl;
+    var image = document.getElementById('armory-cam');
+
+    image.src = imageUrl;
+
+    window.addEventListener('load', maintainSize);
+    window.addEventListener('resize', maintainSize);
+
+  });
+
+  function maintainSize() {
+    var container = document.getElementById('image-container');
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+
+    console.log('maintaining size...');
+    console.log('width', windowWidth);
+    console.log('height', windowHeight);
+
+    container.style.width = windowWidth + 'px';
+    container.style.height = windowHeight + 'px';
+  }
+
 
 })()
